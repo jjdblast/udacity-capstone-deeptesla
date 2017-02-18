7708dd2442f8238612d80c8239b331d79ea4b1f9
jjdblast <jjdblast@gmail.com>
2017-02-18 19:16:47 +0800

    Changes to be committed: 	modified:   __pycache__/params.cpython-34.pyc 	modified:   __pycache__/utils.cpython-34.pyc 	new file:   models/model_best_vgg.h5 	new file:   models/model_best_vgg.json 	new file:   output/epoch10_human_machine.mp4 	modified:   params.py 	modified:   run.py 	modified:   utils.py 	deleted:    visualize.py

--- run.py
+++ run.py
@@ -4,27 +4,43 @@ import os
 import time
 import subprocess as sp
 import itertools
-
+## CV
 import cv2
+## Model
+import numpy as np
 import tensorflow as tf
-
-import params
-import preprocess
-import visualize
-import video_control
-import model
+## Tools
+import utils
+## Parameters
+import params ## you can modify the content of params.py
 
 ## Test epoch
 epoch_ids = [10]
 ## Load model
-model = model.get_model('best_vgg')
+model = utils.get_model('best_vgg')
+
+## Preprocess
+def img_pre_process(img):
+    """
+    Processes the image and returns it
+    :param img: The image to be processed
+    :return: Returns the processed image
+    """
+    ## Chop off 1/3 from the top and cut bottom 150px(which contains the head of car)
+    shape = img.shape
+    img = img[int(shape[0]/3):shape[0]-150, 0:shape[1]]
+    ## Resize the image
+    img = cv2.resize(img, (params.FLAGS.img_w, params.FLAGS.img_h), interpolation=cv2.INTER_AREA)
+    ## Return the image sized as a 4D array
+    return np.resize(img, (params.FLAGS.img_w, params.FLAGS.img_h, params.FLAGS.img_c))
+
 
 ## Process video
 for epoch_id in epoch_ids:
     print('---------- processing video for epoch {} ----------'.format(epoch_id))
-    vid_path = video_control.join_dir(params.data_dir, 'epoch{:0>2}_front.mkv'.format(epoch_id))
+    vid_path = utils.join_dir(params.data_dir, 'epoch{:0>2}_front.mkv'.format(epoch_id))
     assert os.path.isfile(vid_path)
-    frame_count = video_control.frame_count(vid_path)
+    frame_count = utils.frame_count(vid_path)
     cap = cv2.VideoCapture(vid_path)
 
     machine_steering = []
@@ -35,7 +51,7 @@ for epoch_id in epoch_ids:
         ret, img = cap.read()
         assert ret
         ## you can modify here based on your model
-        img = preprocess.img_pre_process(img)
+        img = img_pre_process(img)
         img = img[None,:,:,:]
         deg = float(model.predict(img, batch_size=1))
         machine_steering.append(deg)
@@ -47,7 +63,7 @@ for epoch_id in epoch_ids:
     print('completed inference, total frames: {}, average fps: {} Hz'.format(frame_count, round(fps, 1)))
     
     print('performing visualization...')
-    visualize.visualize(epoch_id, machine_steering, params.out_dir,
+    utils.visualize(epoch_id, machine_steering, params.out_dir,
                         verbose=True, frame_count_limit=None)
     
     
